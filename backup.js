4a07d7c254dd0eb4f44e73f6b4ed8040d4b7468e
chook <dylanw212@gmail.com>
2020-11-21 11:26:55 +1100

    basic site working per spec

--- backup.js
+++ backup.js
@@ -2,24 +2,27 @@ const url = require("url");
 const http = require("http");
 const fileSystem = require("fs");
 
+const indexPage = "./index.html";
+const errorPage = "./404.html";
 const PORT = 8080;
 
 http
   .createServer((req, res) => {
     const urlPath = url.parse(req.url, true);
-    let filename = "." + urlPath.pathname;
+    let filename = "." + urlPath.pathname + ".html";
     console.log(urlPath.pathname);
-    filename = filename === "./" ? "./index.html" : filename;
+    if (urlPath.pathname === "/" || urlPath.pathname === "")
+      filename = indexPage;
+
     fileSystem.readFile(filename, (err, data) => {
       if (err) {
-        errorPage = "./404.html";
-        fileSystem.readFile(errorPage, (err, data) => {
-          if (err) {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            return res.end("404 Not Found");
-          }
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
+        console.log(errorPage);
+        return fileSystem.readFile(errorPage, (err2, errorData) => {
+          if (err2) throw err2;
+
+          console.log(errorPage);
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write(errorData);
           return res.end();
         });
       }
